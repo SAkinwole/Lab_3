07fe8187d69815fce608f62914e4d317a873c5d5
LENOVO <LENOVO@AKINTOBA>
2022-07-18 14:01:33 +0100

    Add project files.

---
+++ Lab_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Prime Number
+            int num;
+            Console.WriteLine("Enter a Number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            int divider = 2;
+            int maxDivider = (int)Math.Sqrt(num);
+            bool prime = true;
+            while (prime && (divider <= maxDivider))
+            {
+                if (num % divider == 0)
+                {
+                    prime = false;
+                }
+                divider++;
+            }
+            Console.WriteLine("Prime? " + prime);
+
+            Console.ReadKey();
+        }
+    }
+}
